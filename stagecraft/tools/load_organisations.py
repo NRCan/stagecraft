1192b86b4dfda165ad141aefd455899bbc558cbf
Tom Booth <tombooth@gmail.com>
2015-02-18 16:03:25 +0000

    WIP

---
+++ stagecraft/tools/load_organisations.py
@@ -0,0 +1,187 @@
+import cPickle as pickle
+import pprint
+import os
+import requests
+import sys
+
+from collections import defaultdict
+from stagecraft.tools.spreadsheets import SpreadsheetMunger
+
+
+def get_govuk_organisations():
+    def get_page(page):
+        response = requests.get(
+            'https://www.gov.uk/api/organisations?page={}'.format(page))
+        return response.json()
+
+    first_page = get_page(1)
+    results = first_page['results']
+
+    for page_num in range(2, first_page['pages'] + 1):
+        page = get_page(page_num)
+        results = results + page['results']
+
+    return results
+
+
+def add_to_slugs(slugs, things):
+    for thing in things:
+        slug = thing[0]
+        if slug in slugs:
+            print '{} exists. offender: {} original: {}'.format(
+                slug, thing, slugs[slug])
+        else:
+            slugs[slug] = thing
+
+
+def load_data(username, password):
+    spreadsheets = SpreadsheetMunger({
+        'names_name': 8,
+        'names_slug': 9,
+        'names_service_name': 6,
+        'names_service_slug': 7,
+        'names_tx_id_column': 18,
+    })
+    #transactions_data = spreadsheets.load(username, password)
+
+    #with open('transactions_data.pickle', 'w') as data_file:
+        #pickle.dump(transactions_data, data_file)
+
+    with open('transactions_data.pickle', 'r') as data_file:
+        transactions_data = pickle.load(data_file)
+
+    #govuk_organisations = get_govuk_organisations()
+
+    #with open('govuk_organisations.pickle', 'w') as org_file:
+        #pickle.dump(govuk_organisations, org_file)
+
+    with open('govuk_organisations.pickle', 'r') as org_file:
+        govuk_organisations = pickle.load(org_file)
+
+    return transactions_data, govuk_organisations
+
+
+def load_organisations(username, password):
+    transactions_data, govuk_organisations = load_data(username, password)
+
+    print transactions_data
+    print govuk_organisations
+
+
+def main():
+    try:
+        username = os.environ['GOOGLE_USERNAME']
+        password = os.environ['GOOGLE_PASSWORD']
+    except KeyError:
+        print "Please supply as environment variables:"
+        print "username (GOOGLE_USERNAME)"
+        print "password (GOOGLE_PASSWORD)"
+        sys.exit(1)
+
+    load_organisations(username, password)
+
+    sys.exit(0)
+
+    # remove any orgs from the list from GOV.UK where they have shut down
+    govuk_organisations = \
+        [org for org in govuk_organisations if org[
+            'details']['closed_at'] is None]
+
+    abbrs = defaultdict(list)
+    types = defaultdict(list)
+
+    for org in govuk_organisations:
+        types[org['format']].append(org)
+        abbr = org['details']['abbreviation']
+        if abbr:
+            abbrs[abbr.lower()].append(org)
+        # else:
+            # print '{} doesnt have an
+            # abbr'.format(unicode(org['title']).encode('ascii','ignore'))
+
+    #abbrs = dict(abbrs)
+    #abbrs_twice = \
+        #{abbr: abbrs[abbr] for abbr, orgs in abbrs.items() if len(orgs) > 1}
+
+    list_abbrs = abbrs.keys()
+    list_abbrs.sort()
+
+    print list_abbrs
+
+    #pprint.pprint(dict(abbrs_twice), width=-1)
+    #pprint.pprint(dict(types), width=-1)
+
+    not_found_depts = set()
+    not_found_agencies = set()
+
+    for transaction in transactions_data:
+        dept_abbr = transaction['department']['abbr']
+        if transaction['agency'] is not None:
+            agency_abbr = transaction['agency']['abbr']
+        else:
+            agency_abbr = None
+
+        dept = abbrs.get(dept_abbr.lower(), None)
+        if dept is None:
+            not_found_depts.add(dept_abbr)
+        elif len(dept) > 1:
+            print 'haerguhaergaegr'
+
+        if agency_abbr:
+            if agency_abbr == 'INSS':
+                agency = abbrs['the insolvency service']
+            else:
+                agency = abbrs.get(agency_abbr.lower(), None)
+                if agency is None:
+                    agency = abbrs.get(transaction['agency']['name'].lower(), None)
+                    if agency is None:
+                        not_found_agencies.add(
+                            (agency_abbr, transaction['agency']['name']))
+                    elif len(agency) > 1:
+                        print 'haerguhaergaegr'
+
+
+    print not_found_depts
+    print not_found_agencies
+
+    # STUFF FOR EXAMINING SPREADSHEET STUFF
+
+    #transactions = set()
+    #services = set()
+    #agencies = set()
+    #departments = set()
+
+    # for transaction in transactions_data:
+    #transactions.add((transaction['slug'], transaction['name']))
+    # services.add(
+    #(transaction['service']['slug'], transaction['service']['name']))
+    # if transaction['agency'] is not None:
+    # agencies.add((transaction['agency']['slug'], transaction[
+    #'agency']['name'], transaction['agency']['abbr']))
+    # departments.add((
+    # transaction['department']['slug'],
+    # transaction['department']['name'],
+    # transaction['department']['abbr']))
+
+    #slugs = dict()
+    #add_to_slugs(slugs, transactions)
+    #add_to_slugs(slugs, services)
+    #add_to_slugs(slugs, agencies)
+    #add_to_slugs(slugs, departments)
+
+    # print len(transactions)
+    # print len(services)
+    # print len(agencies)
+    # print len(departments)
+
+    # print len(transactions) + len(services) + len(agencies) + len(departments)
+    # print len(slugs)
+
+    # print transactions
+    # print services
+    # print agencies
+    # print departments
+
+
+if __name__ == '__main__':
+    main()
---
+++ stagecraft/tools/tests/test_load_organisations.py
@@ -0,0 +1,75 @@
+from hamcrest import (
+    assert_that, equal_to, is_not, is_
+)
+from mock import patch
+
+from stagecraft.apps.dashboards.tests.factories.factories import ModuleFactory
+from stagecraft.apps.datasets.tests.factories import DataSetFactory
+
+from ..load_organisations import load_organisations
+
+
+tx_fixture = [
+    {
+        'name': '',
+        'service': {'name': '', 'slug': ''},
+        'agency': None,
+        'tx_id': '',
+        'department': {'slug': 'home office', 'abbr': 'Home Office', 'name': 'Home Office'},
+        'slug': ''
+    }
+]
+
+govuk_fixture = [
+    {
+        'title': "Attorney General's Office", 
+        'details': {
+            'slug': 'attorney-generals-office',
+            'abbreviation': 'AGO',
+            'closed_at': None,
+        },
+        'parent_organisations': [],
+        'child_organisations': [
+            {
+                'web_url': 'https://www.gov.uk/government/organisations/treasury-solicitor-s-department',
+                'id': 'https://www.gov.uk/api/organisations/treasury-solicitor-s-department'
+            },
+            {
+                'web_url': 'https://www.gov.uk/government/organisations/crown-prosecution-service',
+                'id': 'https://www.gov.uk/api/organisations/crown-prosecution-service'
+            }
+        ]
+    },
+]
+
+
+@patch('stagecraft.tools.load_organisations.load_data')
+def test_load_organisations(mock_load_data):
+    mock_load_data.return_value = tx_fixture, govuk_fixture
+
+    tx_data_set = DataSetFactory(
+        name='transactional_services_summaries',
+    )
+    kpi_module = ModuleFactory(
+        data_set=tx_data_set,
+        query_parameters={
+            'filter_by': ['service_id:daa-reports-filing'],
+        }
+    )
+    dashboard = kpi_module.dashboard
+
+    load_organisations('foo', 'bar')
+
+    assert_that(dashboard.organisation, is_not(None))
+
+    org_parents = dashboard.organisation.get_ancestors(include_self=True)
+
+    assert_that(len(org_parents), is_(4))
+    assert_that(org_parents[0].name, is_('Report filing'))
+    assert_that(org_parents[0].typeOf.name, is_('transaction'))
+    assert_that(org_parents[1].name, is_('Filing cabinet service'))
+    assert_that(org_parents[1].typeOf.name, is_('service'))
+    assert_that(org_parents[2].name, is_('Paper love agency'))
+    assert_that(org_parents[2].typeOf.name, is_('agency'))
+    assert_that(org_parents[3].name, is_('Department of Administrative Affairs'))
+    assert_that(org_parents[3].typeOf.name, is_('department'))
